5e785d9d7cc5fbbaff523c43b39edf44d2d9f013
Charlemagne <lemuelpuglisi001@gmail.com>
2021-05-30 13:38:39 +0200

    feat: predict future RSSI records with sklearn

--- spark/code/structured_streaming_example.py
+++ spark/code/structured_streaming_example.py
@@ -1,15 +1,21 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col, to_timestamp, window
+from pyspark.conf import SparkConf
+from pyspark import SparkContext
+from pyspark.sql.functions import from_json, col, to_timestamp, unix_timestamp, window
 from pyspark.sql.types import *
-import random
+from sklearn.linear_model import LinearRegression
+from elasticsearch import Elasticsearch
+
+
 import pandas as pd
 
 
 def get_spark_session():
-    return SparkSession \
-        .builder \
-        .appName('rssi_signal_prediction') \
-        .getOrCreate()
+    spark_conf = SparkConf()\
+        .set('es.nodes', 'elasticsearch')\
+        .set('es.port', '9200')
+    sc = SparkContext(appName='rssi_signal_prediction', conf=spark_conf)
+    return SparkSession(sc)
 
 
 def get_rds_signal_schema():
@@ -37,71 +43,133 @@ def get_rds_signal_stream(schema: StructType):
         .withColumn('json_content', col('value').cast('string')) \
         .withColumn('content', from_json(col('json_content'), schema)) \
         .select(col('time'), col('content.*')) \
-        .withColumn('_RSSI', col('RSSI').cast('long'))
-
-
-# def regression(dataframe: pd.DataFrame) -> pd.DataFrame:
-#     columns = ['province', 'station_name', 'time', '_RSSI']
-#     if (dataframe.shape[0] < 1):
-#         return pd.DataFrame(columns=columns)
-#     province = dataframe['province'].iloc[0]
-#     station_name = dataframe['station_name'].iloc[0]
-#     # must replace this with actual regression
-#     regression_res = dataframe['_RSSI'].sum()
-#     time = dataframe['time'].max()
-#     data = [province, station_name, time, regression_res]
-#     return pd.DataFrame(data=data, columns=columns)
-
-
-def g(df):
-    grouped = df.groupby(["province", "station_name"])
-    l = []
-    for name, group in grouped:
-        sumOfRSSI = group['_RSSI'].values.sum()
-        time = group['time'].max()
-        stationName = name[1]
-        province = name[0]
-        l.append({
-            "station_name": stationName,
-            "province": province,
-            "_RSSI": sumOfRSSI, 
-            "time": time
-        })
-    return pd.DataFrame(l)
+        .withColumn('milliseconds', unix_timestamp('@timestamp', format="yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")) \
+        .select(
+            col('time'), 
+            col('station_name'), 
+            col('RSSI').cast('long'), 
+            col('@timestamp'), 
+            col('province'), 
+            col('FM'), 
+            col('coords'),
+            col('milliseconds')
+        )
 
 
 def get_resulting_df_schema():
     return  StructType() \
-        .add("province", StringType()) \
-        .add("time", TimestampType()) \
-        .add("station_name", StringType()) \
-        .add("_RSSI", LongType())
+        .add("@timestamp",      StringType()) \
+        .add("station_name",    StringType()) \
+        .add("province",        StringType()) \
+        .add("RSSI",            LongType()) \
+        .add("FM",              StringType()) \
+        .add("coords",          StringType())
+
+
+def get_output_df():
+    return pd.DataFrame(columns=[
+        '@timestamp', 
+        'station_name', 
+        'province', 
+        'RSSI', 
+        'FM', 
+        'coords', 
+    ])
+
+
+def get_linear_regression_model(df: pd.DataFrame):
+    x = df['milliseconds'].to_numpy().reshape(-1, 1)
+    y = df['RSSI'].to_numpy()
+    lr = LinearRegression()
+    lr.fit(x, y)
+    return lr 
+
+
+def make_series(df, timestamp, predicted_rssi) -> pd.Series:
+    return pd.Series([
+        str(timestamp),
+        df.iloc[0]['station_name'],
+        df.iloc[0]['province'],
+        int(predicted_rssi),
+        df.iloc[0]['FM'],
+        df.iloc[0]['coords']
+    ], index=[
+        '@timestamp', 
+        'station_name', 
+        'province', 
+        'RSSI',
+        'FM',
+        'coords', 
+    ])
+
+
+def predict_value(model, milliseconds):
+    rssi_range = lambda s: max(min(0, s), -70)
+    s = model.predict([[milliseconds]])[0]
+    return rssi_range(s)
+    
+
+def predict(df: pd.DataFrame) -> pd.DataFrame:    
+    nrows = lambda df: df.shape[0]    
+    newdf = get_output_df()
+    if (nrows(df) < 1):
+        return newdf
+    model = get_linear_regression_model(df)
+    lastSignalMillisec = df['milliseconds'].values.max()    
+    next_minutes = [ (lastSignalMillisec + (60000 * i)) for i in range(5) ]
+    next_rssi = [predict_value(model, m) for m in next_minutes]
+    for millis, rssi in zip(next_minutes, next_rssi):
+        newdf = newdf.append(make_series(df, millis, rssi), ignore_index=True)
+    return newdf
 
 
 if __name__ == "__main__":
+
     spark = get_spark_session()
-    spark.sparkContext.setLogLevel("WARN")
+    spark.sparkContext.setLogLevel("ERROR")
     schema = get_rds_signal_schema()
     signalStream = get_rds_signal_stream(schema)
-    _schema = get_resulting_df_schema()
 
-    signalStream.groupBy('province', window(signalStream.time, "5 minutes")) \
-        .applyInPandas(g, _schema) \
-        .sum('_RSSI') \
-        .writeStream \
-        .format('console') \
-        .outputMode('append') \
-        .option("truncate", "false") \
-        .start() \
-        .awaitTermination()
+    es_mapping = {
+        "mappings": {
+            "properties": {
+                "@timestamp":   {"type": "date"},
+                "RSSI":         {"type": "long"},
+                "province":     {"type": "keyword"},
+                "station_name": {"type": "keyword"},
+                "FM":           {"type": "keyword"},
+                "coords":       {"type": "text"},
+                "PI":           {"type": "keyword"}
+            }
+        }
+    }
 
-    # timeWindow = window(signalStream.time, '5 minutes')
+    es = Elasticsearch(host='elasticsearch')
+    response = es.indices.create(
+        index='rds-signal-output-spark', 
+        body=es_mapping, 
+        ignore=400
+    )
+
+    if 'acknowledged' in response:
+        if response['acknowledged'] == True:
+            print("Successfully created index:", response['index'])
+
+    win = window(signalStream.time, "1 minutes")
     # signalStream \
-    #     .groupBy('province', 'station_name', timeWindow) \
-    #     .applyInPandas(regression, _schema) \
+    #     .groupBy('province', 'station_name', win) \
+    #     .applyInPandas(predict, get_resulting_df_schema()) \
     #     .writeStream \
     #     .format('console') \
-    #     .outputMode('append') \
-    #     .option('truncate', 'false') \
+    #     .option("truncate", "false") \
     #     .start() \
     #     .awaitTermination()
+
+    signalStream \
+        .groupBy('province', 'station_name', win) \
+        .applyInPandas(predict, get_resulting_df_schema()) \
+        .writeStream \
+        .option('checkpointLocation', '/save/location') \
+        .format('es') \
+        .start('rds-signal-output-spark') \
+        .awaitTermination()
