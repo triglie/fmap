ca8610c4b151dcecfeb91f5293cecd9a0a621ad1
lemuelpuglisi <lemuelpuglisi001@gmail.com>
2021-05-31 00:30:17 +0200

    fix: sending Microsoft WSL team to a forced TSO

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
-DIR="./logs"
+DIR="logs"
 if [ -d "$DIR" ]; then
   echo "Deleting ${DIR}..."
-  sudo rm -R ${DIR}
-else
-  mkdir -p ${DIR}
+  sudo rm -r ${DIR}
 fi
+mkdir logs
 docker-compose build kafkastream
 docker-compose build kafka-to-es
 docker-compose build spark
