710cb1a791e725caa74ff2d19d96e9ec183b3739
Gigi-G <seminara.luigi@gmail.com>
2021-06-06 00:12:16 +0200

    fix: reading serial message

--- arduino/serial_reading.py
+++ arduino/serial_reading.py
@@ -1,8 +1,10 @@
 import serial
 from datetime import datetime
 
+# COM port on windows /dev/tty* on Unix system
+port = "COM3"
 
-ser = serial.Serial('/dev/ttyACM0', 115200, timeout = 1000)
+ser = serial.Serial(port, 115200, timeout = 1000)
 ser.flushInput()
 
 current_date = datetime.now().strftime('%d-%m-%Y')
@@ -13,5 +15,9 @@ f.close()
 while True:
     ser_bytes = ser.readline().decode("utf-8")[:-2] + "\n"
     with open(filename,"a") as f:
-        f.write(ser_bytes)
+        split = ser_bytes.split("FM=")
+        if len(split) > 1 and split[1][0] == "0":
+            ser_bytes = split[0] + "FM=1" + split[1] 
+        if len(ser_bytes) > 5:
+            f.write(ser_bytes)
         
